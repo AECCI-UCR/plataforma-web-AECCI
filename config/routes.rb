8b1f0ac07a52a3dc5d3d120e0830d9c75d1e0b07
Fabian Rodriguez <farodriguez.49@gmail.com>
2016-02-05 18:42:31 -0600

    Primeros cambios, hay un problema con el navbar
    * Se agrega la pagina de “Quienes somos”
    * En la pantalla de inicio se agrega un carousel que eventualmente va a
    mostrar los anuncios
    * Se agrega el navbar

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   root 'static_pages#home'
+
+  get 'about_us' => 'static_pages#about_us'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
