0f7141cd5c52a744cc20e3506808d7608810fae1
Kevin Delgado Sandí <kefdelgado@gmail.com>
2016-02-09 23:21:55 -0600

    Validación de email con dominio ecci.ucr.ac.cr. Bootstrap flash messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
-         :registerable,
+         :confirmable,
          :recoverable,
+         :registerable,
          :rememberable,
          :trackable,
-         :validatable,
-         :confirmable
+         :validatable
 end
