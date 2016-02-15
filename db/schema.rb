889d3e91147cddd6452ac93643350d150d186284
Fabian Rodriguez <farodriguez.49@gmail.com>
2016-02-15 14:42:10 -0600

    Se hacen cambios en la paleta de colores

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,16 @@
 
 ActiveRecord::Schema.define(version: 20160209013252) do
 
+  create_table "ads", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "image_url"
+    t.string   "image_carousel_url"
+    t.boolean  "important"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
