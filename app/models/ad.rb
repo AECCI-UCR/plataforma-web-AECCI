a98daff01b171b5cb3a03ac55814c7efbab0fa16
Fabian Rodriguez <farodriguez.49@gmail.com>
2016-02-22 00:53:28 -0600

    Se añade roles a los anuncios

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -3,4 +3,6 @@ class Ad < ActiveRecord::Base
   mount_uploader :image_carousel_url, ImageUploader
 
   validates :title,  presence: true
+  validates :description, presence: true
+  validates :image_url, presence:true
 end
