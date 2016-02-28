7ed88f51e027c5222d61ce9bd11b82eb10fc9c39
Fabian Rodriguez <farodriguez.49@gmail.com>
2016-02-28 17:57:31 -0600

    Anuncios con markdown :D
    ** Todavía falta revisar xq las tablas no se ver correctamente

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,13 +3,33 @@ module ApplicationHelper
   # Métodos auxiliar para mapear flash key en bootstrap css class
   BOOTSTRAP_FLASH_MSG = {
       notice: 'success',
-      alert:  'danger',
-      error:  'danger'
+      alert: 'danger',
+      error: 'danger'
       #default: 'info'
   }
 
   def bootstrap_class_for(message_type)
     BOOTSTRAP_FLASH_MSG.fetch message_type.to_sym, message_type
+  end
+
+  # Métodos para el parse de markdown a html
+  class CodeRayify < Redcarpet::Render::HTML
+    def block_code(code, language)
+      CodeRay.scan(code, language).div
+    end
+  end
+
+  def markdown(text)
+    coderayified = CodeRayify.new(filter_html: false, hard_wrap: true)
+    options = {
+        fenced_code_blocks: true,
+        no_intra_emphasis: true,
+        autolink: true,
+        lax_html_blocks: true,
+        tables: true,
+    }
+    markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
+    markdown_to_html.render(text).html_safe
   end
 
 end
