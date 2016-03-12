de7b74143f379e9d6fb3ef5a36740d8cbfd23d33
Fabian Rodriguez <farodriguez.49@gmail.com>
2016-03-11 23:08:11 -0600

    Se agrega la vista de profesores y la respectiva ruta

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,11 +1,17 @@
 class CoursesController < ApplicationController
-  before_action :set_course, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!
+  before_action :require_admin
+  before_action :set_course, only: [:show, :edit, :update, :destroy, :profesors]
 
   def index
     @courses = Course.all
   end
 
   def show
+  end
+
+  def profesors
+    @tests = @course.tests.group_by(&:teacher)
   end
 
   def new
@@ -20,13 +26,13 @@ class CoursesController < ApplicationController
     @course.save
 
     #respond_to do |format|
-     # if @course.save
-      #  format.html { redirect_to @course, notice: 'Course was successfully created.' }
-       # format.json { render :show, status: :created, location: @course }
-      # else
-      #  format.html { render :new }
-      #  format.json { render json: @course.errors, status: :unprocessable_entity }
-      #end
+    # if @course.save
+    #  format.html { redirect_to @course, notice: 'Course was successfully created.' }
+    # format.json { render :show, status: :created, location: @course }
+    # else
+    #  format.html { render :new }
+    #  format.json { render json: @course.errors, status: :unprocessable_entity }
+    #end
     #end
   end
 
@@ -51,13 +57,14 @@ class CoursesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_course
-      @course = Course.find(params[:id])
-    end
+# Use callbacks to share common setup or constraints between actions.
+  def set_course
+    @course = Course.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def course_params
-      params.require(:course).permit(:name, :initials)
-    end
+# Never trust parameters from the scary internet, only allow the white list through.
+  def course_params
+    params.require(:course).permit(:name, :initials)
+  end
+
 end
