ed38bc7ffd8594c34abc8b792ffd69b692d73bf8
Fabian Rodriguez <farodriguez.49@gmail.com>
2016-03-25 10:18:56 -0600

    Mensaje de error para los archivos

--- app/models/test.rb
+++ app/models/test.rb
@@ -6,14 +6,16 @@ class Test < ActiveRecord::Base
   # Scopes
   scope :order_for_table, lambda { joins(:course, :teacher).order('courses.initials', 'teachers.name', year: :desc, semester: :asc, test_number: :asc) }
 
+  # CarrierWeaver
+  mount_uploader :file_url, PdfUploader
+
   # Validations
   validates :year, presence: true
   validates :teacher_id, presence: true
   validates :semester, presence: true
   validates :test_number, presence: true
-  validates :file_url, presence: true, file_size: {less_than_or_equal_to: 20.megabytes}
+  validates :file_url, presence: true, file_size: {less_than_or_equal_to: 20.megabytes, message: 'El archivo debe ser menor a %{count}.'}
   validates :course_id, presence: true
 
-  # CarrierWeaver
-  mount_uploader :file_url, PdfUploader
+
 end
