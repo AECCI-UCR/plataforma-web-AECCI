ea5e7ab31ae2e2faeba7ed5193d1fa2526a5b838
Fabian Rodriguez <farodriguez.49@gmail.com>
2016-03-28 23:45:02 -0600

    Validaciones y respectivos errores

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -2,7 +2,8 @@ class Ad < ActiveRecord::Base
   mount_uploader :image_url, ImageUploader
   mount_uploader :image_carousel_url, ImageUploader
 
-  validates :title,  presence: true
+  # Validations
+  validates :title, presence: true
   validates :description, presence: true
-  validates :image_url, presence: true
+  validates :image_url, presence: true, file_content_type: {allow: ['image/jpeg', 'image/gif', 'image/png', 'image/jpg'], mode: :strict}
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -9,12 +9,17 @@ class Test < ActiveRecord::Base
   # CarrierWeaver
   mount_uploader :file_url, PdfUploader
 
+  # Para los rangos de los semestres
+  max_semester = 3
+  min_semester = 1
+
   # Validations
   validates :year, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 1981, less_than_or_equal_to: Time.now.year}
   validates :teacher_id, presence: true
-  validates :semester, presence: true, numericality: {only_integer: true, greater_than: 0}
+  validates :semester, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: min_semester, less_than_or_equal_to: max_semester}
   validates :test_number, presence: true, numericality: {only_integer: true, greater_than: 0}
-  validates :file_url, presence: true, file_size: {less_than_or_equal_to: 20.megabytes, message: ' debe ser menor a %{count}.'}
+  validates :file_url, presence: true, file_size: {less_than_or_equal_to: 20.megabytes, message: ' debe ser menor a %{count}'}, file_content_type: {allow: 'application/pdf', mode: :strict}
   validates :course_id, presence: true
+
 
 end
