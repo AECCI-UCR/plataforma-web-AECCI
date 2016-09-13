9d779f63225440723494dfde1824cd8f6aa8e34c
Fabian <farodriguez.49@gmail.com>
2016-09-13 21:30:54 +0000

    chore: COnfiguración Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,14 +26,13 @@ lock '3.6.0'
 
 set :application,     'plataforma-web-AECCI'
 set :repo_url,        'git@github.com:AECCI-UCR/plataforma-web-AECCI.git'
-set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 
 set :pty,             true
 set :use_sudo,        false
 set :deploy_via,      :remote_cache
 
 ## Linked Files & Directories:
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 set :linked_files, %w{config/database.yml config/secrets.yml}
 
 # rbenv
@@ -47,3 +46,38 @@ set :rbenv_roles, :all
 set :puma_workers, 0
 set :puma_threads, [0, 4]
 set :puma_init_active_record, true
+
+# Otros
+namespace :puma do
+  desc 'Create Directories for Puma Pids and Socket'
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
+    end
+  end
+
+  before :start, :make_dirs
+end
+
+namespace :deploy do
+  desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
+    end
+  end
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      invoke 'puma:restart'
+    end
+  end
+
+  before :starting,     :check_revision
+  after  :finishing,    :compile_assets
+  after  :finishing,    :cleanup
+  after  :finishing,    :restart
+end
