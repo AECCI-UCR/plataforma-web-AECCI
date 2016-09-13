8ebc4099b27986726f8e732078648d27da55a735
Fabian Rodriguez <farodriguez.49@gmail.com>
2016-09-13 16:13:42 -0600

    Se elimina la dependencia de las gemas de GitHub
    El servidor de la AECCI tiene bloqueada la salida a GitHub, por lo que se tuvo que poner que las gemas no las bajaran de este sitio.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,6 @@
 set :stage, :production
 set :rails_env, :production
 
+set :user, "asoinfo"
 set :deploy_to, "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 server '10.1.104.157', user: 'asoinfo', roles: %w{app db web}, primary: true
