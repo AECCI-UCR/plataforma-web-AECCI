945b7dda1086f1e81706d4df1a08f662c33d60ee
Fabian Rodriguez <farodriguez.49@gmail.com>
2016-10-19 20:28:24 -0600

    Se actualiza la configuracion de nginx

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,4 +3,4 @@ set :rails_env, :production
 
 set :user, "asoinfo"
 set :deploy_to, "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
-server '10.1.104.157', user: 'asoinfo', roles: %w{app db web}, primary: true
+server '163.178.104.157', user: 'asoinfo', roles: %w{app db web}, primary: true
